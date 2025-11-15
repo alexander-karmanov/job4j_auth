e3cd7cfce08aca0e4f5eafe361acd0ce6299236f
alexander-karmanov <lektor_md@mail.ru>
2025-11-15 16:07:32 +0300

    1. RESTFul. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -0,0 +1,62 @@
+package ru.job4j.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.domain.Person;
+import ru.job4j.repository.PersonRepository;
+import ru.job4j.service.PersonService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+
+        this.personService = personService;
+    }
+
+    @GetMapping("/")
+    public List<Person> findAll() {
+        return this.personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable int id) {
+        var person = this.personService.findById(id);
+        return new ResponseEntity<Person>(
+                person.orElse(new Person()),
+                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Person> create(@RequestBody Person person) {
+        return new ResponseEntity<Person>(
+                this.personService.create(person),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> update(@RequestBody Person person) {
+        Person existing = this.personService.findById(person.getId()).orElse(null);
+        if (existing != null && existing.equals(person)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        this.personService.delete(id);
+        if (this.personService.findById(id).isPresent()) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/ru/job4j/service/PersonService.java
@@ -0,0 +1,37 @@
+package ru.job4j.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.domain.Person;
+import ru.job4j.repository.PersonRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PersonService {
+
+    private final PersonRepository repository;
+
+    public List<Person> findAll() {
+        return (List<Person>) repository.findAll();
+    }
+
+    public Optional<Person> findById(int id) {
+        return repository.findById(id);
+    }
+
+    public Person create(Person person) {
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+        return repository.save(person);
+    }
+
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
+
+}
